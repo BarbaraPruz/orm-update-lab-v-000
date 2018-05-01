ed5f5552dc869d55c97fea98f7eabe333f5b792f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-01 23:15:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
       update
     else
       sql = "INSERT INTO students (name, grade) VALUES (?,?)"
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
