c47d12fe1652b11d7b32d8713b0962feafec5ce2
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-01 23:16:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_reader :id, :name, :grade
-
+  attr_writer  :name, :grade       # for test; another approach is to implement writer that includes save
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize (name, grade, id=nil)
