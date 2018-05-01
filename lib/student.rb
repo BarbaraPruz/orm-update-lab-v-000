adb99a29ccae84bc26247b6aefdacbb252c7c5df
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-01 23:18:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,8 +53,7 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name=?"
-    binding.pry
-    row = DB[:conn].execute(sql,name)
+    row = DB[:conn].execute(sql,name).first
     self.new_from_db(row)
   end
 end
